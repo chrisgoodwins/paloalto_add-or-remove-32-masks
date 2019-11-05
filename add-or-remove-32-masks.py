f5d36e3f24246dd04538d7f22fb0b73dd0eef95b
Chris Goodwin <35201787+chrisgoodwins@users.noreply.github.com>
2019-11-04 23:31:20 -0800

    Update add-or-remove-32-masks.py

--- add-or-remove-32-masks.py
+++ add-or-remove-32-masks.py
@@ -1,3 +1,20 @@
+###############################################################################
+#
+# Script:       add-or-remove-32-masks.py
+#
+# Author:       Chris Goodwin <cgoodwin@paloaltonetworks.com>
+#
+# Description:  Standardize /32 subnet masks across all host IP address objects
+#               for a Palo Alto Networks firewall or Panorama device group. Run
+#               the script against a live device, or against at config file.
+#
+# Requirements: requests
+#
+# Python:       Version 2
+#
+###############################################################################
+###############################################################################
+
 
 import getpass
 import sys
@@ -10,8 +27,9 @@ try:
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 except ImportError:
     raise ValueError('requests support not available, please install module')
-############################################################################
-############################################################################
+
+###############################################################################
+###############################################################################
 
 
 # Prompts the user to enter the IP/FQDN of a firewall to retrieve the api key
@@ -89,8 +107,11 @@ def getDG(fwip, mainkey, devTree):
         dgXmlUrl = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/device-group&key=%s" % (fwip, mainkey)
         r = requests.get(dgXmlUrl, verify=False)
         devTree = ET.fromstring(r.text)
+        devTreeString = './/device-group/entry'
+    else:
+        devTreeString = 'devices/entry/device-group/entry'
     dgList = []
-    for entry in devTree.findall('.//device-group/entry'):
+    for entry in devTree.findall(devTreeString):
         dgList.append(entry.get('name'))
     while True:
         try:
@@ -133,28 +154,36 @@ def getAddressObjects(fwip, mainkey, dg, devTree):
             devURL = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/vsys/entry/address&key=%s" % (fwip, mainkey)
             r = requests.get(devURL, verify=False)
             devTree = ET.fromstring(r.text)
-        else:
             addrList = devTree.findall('.//address/entry')
+        else:
+            addrList = devTree.findall('./devices/entry/vsys/entry/address/entry')
     else:
         if devTree is None:
             devURL = "https://%s/api/?type=config&action=get&xpath=/config/devices/entry/device-group/entry[@name='%s']/address&key=%s" % (fwip, dg, mainkey)
             r = requests.get(devURL, verify=False)
             devTree = ET.fromstring(r.text)
+            addrList = devTree.findall('.//address/entry')
         else:
-            addrList = devTree.findall(".//device-group/entry[@name='%s']/address/entry" % (dg))
+            addrList = devTree.findall("./devices/entry/device-group/entry[@name='%s']/address/entry" % (dg))
     return addrList
 
 
 # Pushes the API call to firewall or Panorama
-def pushApiCall(fwip, mainkey, dg, newAddrObjString):
-    raw_input('\nPress Enter to push API calls to Panorama/firewall (or CTRL+C to kill the script)... ')
-    if dg is None:
-        devURL = "https://%s/api/?type=config&action=set&xpath=/config/devices/entry/vsys/entry/address&element=%s&key=%s" % (fwip, newAddrObjString, mainkey)
+def pushAddrChanges(fwip, mainkey, dg, devTree, newAddrObjString):
+    if devTree is None:
+        raw_input('\n\nPress Enter to push API calls to Panorama/firewall (or CTRL+C to kill the script)... ')
+        if dg is None:
+            devURL = "https://%s/api/?type=config&action=set&xpath=/config/devices/entry/vsys/entry/address&element=%s&key=%s" % (fwip, newAddrObjString, mainkey)
+        else:
+            devURL = "https://%s/api/?type=config&action=set&xpath=/config/devices/entry/device-group/entry[@name='%s']/address&element=%s&key=%s" % (fwip, dg, newAddrObjString, mainkey)
+        r = requests.get(devURL, verify=False)
+        tree = ET.fromstring(r.text)
+        status = tree.get('status')
     else:
-        devURL = "https://%s/api/?type=config&action=set&xpath=/config/devices/entry/device-group/entry[@name='%s']/address&element=%s&key=%s" % (fwip, dg, newAddrObjString, mainkey)
-    r = requests.get(devURL, verify=False)
-    tree = ET.fromstring(r.text)
-    status = tree.get('status')
+        raw_input('\n\nPress Enter to push changes to Panorama/firewall config (or CTRL+C to kill the script)... ')
+        time.sleep(1)
+        status = 'success'
+        devURL = None
     return status, devURL
 
 
@@ -170,13 +199,11 @@ def remove32s(fwip, mainkey, dg, devTree):
             newAddrObjString = newAddrObjString + ET.tostring(entry)
     if newAddrObjString == '':
         time.sleep(1)
-        print('\n\nThere are no address objects fitting your criteria...\n\nGoodbye!!!\n\n\n')
-        exit()
-    if devTree is None:
-        status, devURL = pushApiCall(fwip, mainkey, dg, newAddrObjString)
+        print('\n\nThere are no address objects fitting your criteria...\n\n')
+        time.sleep(1)
+        status = 'fail'
     else:
-        status = 'success'
-        devTree = None
+        status, devURL = pushAddrChanges(fwip, mainkey, dg, devTree, newAddrObjString)
     return status, devTree
 
 
@@ -192,14 +219,43 @@ def add32s(fwip, mainkey, dg, devTree):
             newAddrObjString = newAddrObjString + ET.tostring(entry)
     if newAddrObjString == '':
         time.sleep(1)
-        print('\n\nThere are no address objects fitting your criteria...\n\nGoodbye!!!\n\n\n')
-        exit()
-    if devTree is None:
-        status, devURL = pushApiCall(fwip, mainkey, dg, newAddrObjString)
+        print('\n\nThere are no address objects fitting your criteria...\n\n')
+        time.sleep(1)
+        status = 'fail'
     else:
-        status = 'success'
-        devTree = None
+        status, devURL = pushAddrChanges(fwip, mainkey, dg, devTree, newAddrObjString)
     return status, devTree
+
+
+# Presents the user with the option to add or remove /32 subnet masks
+def addRemoveChoice(fwip, mainkey, dg, devTree):
+    while True:
+        userChoice = raw_input('\n\nWould you like to add or remove /32 subnet masks?\n\n1) Add /32 subnet masks\n2) Remove /32 subnet masks\n\nAnswer: ')
+        if userChoice == '1':
+            status, result = add32s(fwip, mainkey, dg, devTree)
+            break
+        elif userChoice == '2':
+            status, result = remove32s(fwip, mainkey, dg, devTree)
+            break
+        else:
+            print("\n\nChoose '1' or '2', try again...\n")
+            time.sleep(1)
+    return status, result
+
+
+# Checks the status from the changes that were made from API call or config change
+def checkStatus(devTree, status, result):
+    if status == 'success':
+        if devTree is None:
+            print('\n\nYour changes were successfully pushed to your PAN device')
+        else:
+            print('\n\nYour changes were successfully written to config')
+    elif status == 'fail':
+        return status
+    else:
+        print('\n\nSomething went wrong while pushing your config to the firewall/Panorama\n\n')
+        print('Here is the faulty API call:\n\n' + result)
+        exit()
 
 
 def main():
@@ -215,29 +271,36 @@ def main():
         devTree = panConfig.getroot()
         print('\n\n\n...Device config loaded from command argument...')
     devType = getDevType(fwip, mainkey, devTree)
-    if devType == 'pano':
-        dg = getDG(fwip, mainkey, devTree)
-    while True:
-        userChoice = raw_input('\n\nWould you like to add or remove /32 subnet masks?\n\n1) Add /32 subnet masks\n2) Remove /32 subnet masks\n\nAnswer: ')
-        if userChoice == '1':
-            status, result = add32s(fwip, mainkey, dg, devTree)
-            break
-        elif userChoice == '2':
-            status, result = remove32s(fwip, mainkey, dg, devTree)
-            break
+    run = True
+    while run is True:
+        if devType == 'pano':
+            dg = getDG(fwip, mainkey, devTree)
+            status, result = addRemoveChoice(fwip, mainkey, dg, devTree)
+            if status == 'success':
+                successCheck = True
+            checkStatus(devTree, status, result)
+            while True:
+                dgChoice = raw_input('\n\nWould you like to run this script against another device group? [Y/n]  ')
+                if dgChoice == '' or dgChoice == 'Y' or dgChoice == 'y':
+                    break
+                elif dgChoice == 'N' or dgChoice == 'n':
+                    run = False
+                    break
+                else:
+                    print("\n\nChoose '1' or '2', try again...\n")
+                    time.sleep(1)
         else:
-            print("\n\nChoose '1' or '2', try again...\n")
-            time.sleep(1)
-    if status == "success":
-        if devTree is None:
-            print('\n\nYour changes were successfully pushed to your PAN device\n\n\nHave a great day!!\n\n')
-        else:
-            raw_input('\nPress Enter to push changes to Panorama/firewall config (or CTRL+C to kill the script)... ')
-            panConfig.write('EDITED_BY_SCRIPT_' + sys.argv[1])
-            print('\n\nYour changes were successfully made, and your config was saved as EDITED_BY_SCRIPT_' + sys.argv[1])
-    else:
-        print('\n\nSomething went wrong while pushing your config to the firewall\n\n')
-        print('Here is the faulty API call:\n\n' + result)
+            status, result = addRemoveChoice(fwip, mainkey, dg, devTree)
+            if checkStatus(devTree, status, result) == 'fail':
+                continue
+            else:
+                successCheck = True
+                run = False
+    if devTree is not None and successCheck is True:
+        print('\nWriting config to file. Please hold....\n')
+        panConfig.write('EDITED_BY_SCRIPT_' + sys.argv[1])
+        print('\n\n\nYour config was saved as EDITED_BY_SCRIPT_' + sys.argv[1])
+    print('\n\n\nHave a great day!!\n\n')
 
 
 if __name__ == '__main__':
